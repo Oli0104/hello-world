062270e55bdacec6d134b9c6cd2163dee79d3f3f
swamib18 <swamib18@htl-kaindorf.at>
2022-05-25 16:01:11 +0200

    Änderung Michael

--- src/main/java/Hello_World.java
+++ src/main/java/Hello_World.java
@@ -5,5 +5,6 @@
 public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello from Admiral Puntigam");
     }
 }
